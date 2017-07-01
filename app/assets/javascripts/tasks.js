9d0f9840b629748cacb33eb41d95cb89e132efd8
Julio Berina <julioberina@gmail.com>
2017-07-01 07:45:59 +0000

    Added strikethrough feature to show that the task has been completed

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -5,7 +5,9 @@
     // <li> tags
     function taskHtml(task) {
       var checkedStatus = task.done ? "checked " : "";
-      var liElement = '<li><div class="view"><input class="toggle" type="checkbox" '
+      var liClass = task.done ? "completed" : "";
+      var liElement = '<li class="' + liClass + '">' +
+      '<div class="view"><input class="toggle" type="checkbox" '
       + 'data-id="' + task.id + '" ' + checkedStatus +
       '/><label>' +
       task.title + '</label></div></li>';
@@ -26,6 +28,11 @@
         task: {
           done: doneValue
         }
+      }).success(function(data) {
+        var liHtml = taskHtml(data);
+        var $li = $("#listItem-" + data.id);
+        $li.replaceWith(liHtml);
+        $('.toggle').change(toggleTask);
       });
     }
 
